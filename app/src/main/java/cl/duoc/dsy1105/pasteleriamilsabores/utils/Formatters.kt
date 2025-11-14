7c0a00132fcc1a4b743eba71ac5036cab5fadbe7
catluvies <anyararosso@gmail.com>
2025-11-14 18:17:53 -0300

    refactor: mueve formateador de precios a un objeto singleton

--- app/src/main/java/cl/duoc/dsy1105/pasteleriamilsabores/utils/Formatters.kt
+++ app/src/main/java/cl/duoc/dsy1105/pasteleriamilsabores/utils/Formatters.kt
@@ -3,9 +3,9 @@ package cl.duoc.dsy1105.pasteleriamilsabores.utils
 import java.text.NumberFormat
 import java.util.Locale
 
-fun formatPriceChilean(price: Number): String {
-    val locale = Locale.Builder().setLanguage("es").setRegion("CL").build()
-    val format = NumberFormat.getCurrencyInstance(locale)
-    format.maximumFractionDigits = 0
-    return format.format(price)
+object Formatters {
+
+    val clPriceFormatter: NumberFormat = NumberFormat.getCurrencyInstance(
+        Locale.Builder().setLanguage("es").setRegion("CL").build()
+    )
 }
